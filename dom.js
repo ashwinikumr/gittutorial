86eee8284780f1b385dfeb1f8f0d3f8f5d251901
ashwinikumr <kumar1991ashwini@gmail.com>
2023-12-12 21:37:29 +0530

    added demo.js file

---
+++ dom.js
@@ -0,0 +1,27 @@
+
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title=123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent="hello";
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+// console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+var header=document.getElementById('main-header');
+// console.log(headerTitle);
+// headerTitle.textContent='Hello';
+// headerTitle.innerHTML='GoodBye';
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML='<h3>Hello</h3>';
+// header.style.borderBottom='solid 3px #000';
+
+
